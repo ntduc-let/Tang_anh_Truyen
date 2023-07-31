b636430acf67183fd49283fc6f32710ece54d767
ntduc_let <savatar2204@gmail.com>
2023-07-31 11:17:35 +0700

    update base

--- app/src/main/java/com/android/ntduc/baseproject/App.kt
+++ app/src/main/java/com/android/ntduc/baseproject/App.kt
@@ -2,12 +2,10 @@ package com.android.ntduc.baseproject
 
 import android.app.Application
 import com.orhanobut.hawk.Hawk
-import com.proxglobal.ads.application.ProxApplication
-import com.proxglobal.proxads.ads.application.AdsApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-open class App : AdsApplication() {
+open class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Hawk.init(this).build()
